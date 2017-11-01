a2f5c200417537b8650be3a8561bde6469afbabb
markandirt <markandirt@gmail.com>
2017-11-01 20:14:34 +0100

    Country List

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -2,12 +2,47 @@ package edu.upc.eseiaat.pma.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CountryListActivity extends AppCompatActivity {
+
+    private ArrayList<String> country_list;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_list);
+
+        String[]  countries = getResources().getStringArray(R.array.countries);
+        country_list = new ArrayList<>(Arrays.asList(countries));
+
+        ListView list = (ListView) findViewById(R.id.country_list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, country_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View item, int pos, long id) {
+                Toast.makeText(
+                        CountryListActivity.this,
+                        String.format("Has escogido '%s'", country_list.get(pos)),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
     }
 }
